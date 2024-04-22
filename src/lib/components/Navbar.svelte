6decc31f82eb49fb1b31d4121e3ac0bf539ff512
mouise1111 <mouise1111@gmail.com>
2024-04-22 17:26:10 +0200

    added navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<div class="border-b-2 navbar bg-base-100 border-neutral">
+	<div class="flex-1">
+		<a class="text-xl btn btn-ghost">ArabicMastery</a>
+	</div>
+	<div class="flex-none">
+		<ul class="justify-center px-1 space-x-2 align-middle menu menu-horizontal lg:text-lg">
+			<li>
+				<details>
+					<summary> Programs </summary>
+					<ul class="p-2 rounded-t-none bg-base-100">
+						<li><a>Grammer</a></li>
+						<li><a>Vocabulary</a></li>
+						<li><a>Speaking</a></li>
+					</ul>
+				</details>
+			</li>
+			<li><a class="btn btn-primary">Subscribe</a></li>
+			<li><a>Login</a></li>
+		</ul>
+	</div>
+</div>
