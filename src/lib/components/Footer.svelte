e5e261fa5cc2a4dfa9b8da7283ce65fd9e6e4afa
mouise1111 <mouise1111@gmail.com>
2024-04-25 11:48:59 +0200

    Add Footer component with useful links, programs, and social media links

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,28 @@
+<footer class="border-t footer bg-base-100 border-neutral">
+	<div class="flex flex-col justify-between w-full lg:flex-row lg:px-64 lg:py-8 lg:text-lg">
+		<div>
+			<h2 class="text-xl font-bold lg:text2xl text-neutral">Useful Links</h2>
+			<ul class="mt-2 space-y-2">
+				<li><a href="#" class="text-neutral-light hover:text-primary">About Us</a></li>
+				<li><a href="#" class="text-neutral-light hover:text-primary">Contact Us</a></li>
+				<li><a href="#" class="text-neutral-light hover:text-primary">Privacy Policy</a></li>
+			</ul>
+		</div>
+		<div>
+			<h2 class="text-xl font-bold text-neutral">Programs</h2>
+			<ul class="mt-2 space-y-2">
+				<li><a href="#" class="text-neutral-light hover:text-primary">Grammer</a></li>
+				<li><a href="#" class="text-neutral-light hover:text-primary">Vocabulary</a></li>
+				<li><a href="#" class="text-neutral-light hover:text-primary">Speaking</a></li>
+			</ul>
+		</div>
+		<div>
+			<h2 class="text-xl font-bold text-neutral">Connect With Us</h2>
+			<ul class="mt-2 space-y-2">
+				<li><a href="#" class="text-neutral-light hover:text-primary">Facebook</a></li>
+				<li><a href="#" class="text-neutral-light hover:text-primary">Twitter</a></li>
+				<li><a href="#" class="text-neutral-light hover:text-primary">Instagram</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
